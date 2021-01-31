e46c20257d3ec91fe87f7cb2ad36b2ea0a5fcfb1
kquach229 <kquach229@gmail.com>
2021-01-31 00:38:51 -0500

    complete

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use("cors");
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello");
